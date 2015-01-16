c90ab6f6446ab5bfd89c6c21e1d39524675fb40e
Bill Waggoner <ctgreybeard@gmail.com>
2015-01-16 12:07:45 -0500

    Initial Commit

---
+++ StarshipSimulation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StarshipSimulation
+//
+//  Created by William Waggoner on 1/16/15.
+//  Copyright (c) 2015 William C Waggoner. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
